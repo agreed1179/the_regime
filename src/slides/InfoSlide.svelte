566565fc6a59b8cf7976786d380245d3d3b51bf8
Agreed <lzy@hotmail.com>
2024-11-13 06:19:32 +1100

    added a lot of background images to chapters 1-3

--- src/slides/InfoSlide.svelte
+++ src/slides/InfoSlide.svelte
@@ -87,8 +87,7 @@
       left: 0;
       width: 100%;
       height: 100%;
-      background: rgba(0, 0, 0, 0.4);
-      z-index: 1;
+      background: rgba(0, 0, 0, 0);   /* disabled for full transparency */
   }
 
   /* Info Box Styling */
